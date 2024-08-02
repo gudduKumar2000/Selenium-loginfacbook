84a63bf05b93706034f2db0ab89903f7e14cf1a3
gudduKumar2000 <gudduaur2016@gmail.com>
2024-08-02 14:48:41 +0530

    addedfile

---
+++ Facebook_login.java
@@ -0,0 +1,29 @@
+package facebook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebook_login 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
+		
+		ChromeDriver driver = new ChromeDriver();
+      driver.manage().window().maximize();
+          Thread.sleep(2000);
+       driver.get("https://www.facebook.com/");
+       Thread.sleep(2000);
+      WebElement ele= driver.findElement(By.xpath("//input[@name='email']"));
+     	Thread.sleep(2000);
+     	ele.sendKeys("7903387778");
+     	Thread.sleep(2000);
+     	ele.sendKeys(Keys.TAB+"Guddu@123",Keys.ENTER);
+    	Thread.sleep(2000);
+     	//WebElement ele2= driver.findElement(By.xpath("//input[@name='pass']"));
+     	 // ele.sendKeys("Guddu@123");
+     		//ele.sendKeys(Keys.ENTER);
+}
+}
